a98b18c01f505d6167597bcc34a2f938cd2287d0
workamirhossein <workamirhossein@gmail.com>
2022-06-14 17:52:42 +0430

    btn has been made

---
+++ src/components/buttons/Button.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './Button.css';
+import { Link } from "react-router-dom";
+
+// define button size and style class for styling
+const STYLES = ['btn--primary', 'btn--outline'];
+const SIZES = ['btn--medium', 'btn--large'];
+
+// passing the datas
+export const Buttons = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize,
+}) => {
+    //set type of btn
+    const checkButtonStyle = STYLES.includes( buttonStyle ) ? buttonStyle : STYLES[0];
+    const checkButtonSize = STYLES.includes( buttonSize ) ? buttonSize : STYLES[0];
+
+    //making btn
+    return (
+        <Link to='/sign-up' className='btn-mobile'>
+            <button
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+             onClick={onClick}  type={type} >
+                {children}
+            </button>
+        </Link>
+    )
+
+}
+
